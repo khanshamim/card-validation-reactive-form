a824b7e946a29cc7ec6c5940b504f3ebeee8afaf
Shamim Khan <cc1254@dupont.com>
2018-10-29 13:47:55 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,86 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpModule
+      ],
+      providers: []
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+
+
+  it('form should be invalid when empty', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    const app = fixture.componentInstance;
+    app.ngOnInit();
+    expect(app.paymentForm.valid).toBeFalsy();
+  });
+
+  it('card number validation should work', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.ngOnInit();
+    app.paymentForm.controls['cardNumber'].setValue("1234123412345678");
+    expect(app.paymentForm.controls['cardNumber'].valid).toBeTruthy();
   }));
-  it(`should have as title 'payment-gateway'`, async(() => {
+
+  it('expiration date validation should work', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('payment-gateway');
+    const app = fixture.componentInstance;
+    app.ngOnInit();
+    app.paymentForm.controls['expirationMonth'].setValue("134");
+    app.paymentForm.controls['expirationYear'].setValue("2017");
+    expect(app.paymentForm.controls['expirationMonth'].valid).toBeFalsy();
+    expect(app.paymentForm.controls['expirationYear'].valid).toBeTruthy();
   }));
-  it('should render title in a h1 tag', async(() => {
+
+  it('cvv/cvc validation should work', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to payment-gateway!');
+    const app = fixture.componentInstance;
+    app.ngOnInit();
+    app.paymentForm.controls['cardCVVNumber'].setValue("677");
+    expect(app.paymentForm.controls['cardCVVNumber'].valid).toBeTruthy();
+  }));
+
+  it('submit button should display the success message', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.ngOnInit();
+    app.paymentForm.controls['nameOnCard'].setValue("Mathew Jerome");
+    app.paymentForm.controls['cardNumber'].setValue("3456234567854567");
+    app.paymentForm.controls['expirationMonth'].setValue("11");
+    app.paymentForm.controls['expirationYear'].setValue("2017");
+    app.paymentForm.controls['cardCVVNumber'].setValue("677");
+    app.submitForm();
+    expect(app.displayMessage).toBe("Payment Successful!");
+  }));
+
+  it('submit button should display the failure message', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.ngOnInit();
+    app.paymentForm.controls['nameOnCard'].setValue("Mathew Jerome");
+    app.paymentForm.controls['cardNumber'].setValue("345623456784567");
+    app.paymentForm.controls['expirationMonth'].setValue("15");
+    app.paymentForm.controls['expirationYear'].setValue("2017");
+    app.paymentForm.controls['cardCVVNumber'].setValue("6776");
+    app.submitForm();
+    expect(app.displayMessage).toBe("Payment Failed!");
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'payment-form',
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'payment-gateway';
+
+export class AppComponent implements OnInit {
+  paymentForm: FormGroup;
+  displayMessage: string;
+
+  constructor(private formBuilder: FormBuilder) {
+
+    /* Declare Reactive Form Group here */
+
+  }
+
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.paymentForm = this.formBuilder.group({
+      nameOnCard: ['', [Validators.required,Validators.minLength(1),Validators.pattern('^[A-Za-z][A-Za-z -]*$')]],
+      cardNumber: ['', [Validators.required,Validators.minLength(16),Validators.min(1111111111111111),Validators.max(9999999999999999)]],
+      expirationMonth: ['', [Validators.required,Validators.minLength(1),Validators.maxLength(2),Validators.min(1),Validators.max(12)]],
+      expirationYear: ['', [Validators.required,Validators.minLength(4),Validators.maxLength(4),Validators.min(1111),Validators.max(9999)]],
+      cardCVVNumber: ['', [Validators.required,Validators.minLength(3),Validators.maxLength(3),Validators.min(111),Validators.max(999)]]
+    });
+  }
+
+ // convenience getter for easy access to form fields
+ get f() { return this.paymentForm.controls; }
+
+ onSubmit() {
+  this.submitForm();
+  }
+
+  submitForm() {
+    /* Change the display message on button click / submit form */
+  // stop here if form is invalid
+  if (this.paymentForm.invalid) {
+    this.displayMessage = "Payment Failed!";
+      return;
+  }
+    this.displayMessage = "Payment Successful!";
+  }
 }
